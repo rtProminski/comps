60bbebbd384d3319c61fa077e15e9c4164027294
Robert <rt.prominski@gmail.com>
2023-03-16 11:56:11 +0100

    changing the folder structure

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import Button from './Button'
-import { HiStar } from 'react-icons/hi'
+import ButtonPage from './pages/ButtonPage'
 
-function App() {
-	const handleClick = () => {
-		alert('click button')
-	}
-
+const App = () => {
 	return (
 		<div>
-			<div>
-				<Button secondary outline rounded addedStyles={'mb-5 ml-3 mt-2'}>
-					<HiStar />
-					Click me!!
-				</Button>
-			</div>
-			<div>
-				<Button danger outline>
-					<HiStar />
-					Buy Now!
-				</Button>
-			</div>
-			<div>
-				<Button warning>
-					<HiStar />
-					See Deal!
-				</Button>
-			</div>
-			<div>
-				<Button secondary outline>
-					<HiStar />
-					Hide Ads!
-				</Button>
-			</div>
-			<div>
-				<Button onClick={handleClick} primary rounded>
-					<HiStar />
-					Something!
-				</Button>
-			</div>
+			<ButtonPage />
 		</div>
 	)
 }
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,45 @@
+import Button from '../components/Button'
+import { HiStar } from 'react-icons/hi'
+
+const ButtonPage = () => {
+	const handleClick = () => {
+		alert('click button')
+	}
+
+	return (
+		<div>
+			<div>
+				<Button secondary outline rounded addedStyles={'mb-5 ml-3 mt-2'}>
+					<HiStar />
+					Click me!!
+				</Button>
+			</div>
+			<div>
+				<Button danger outline>
+					<HiStar />
+					Buy Now!
+				</Button>
+			</div>
+			<div>
+				<Button warning>
+					<HiStar />
+					See Deal!
+				</Button>
+			</div>
+			<div>
+				<Button secondary outline>
+					<HiStar />
+					Hide Ads!
+				</Button>
+			</div>
+			<div>
+				<Button onClick={handleClick} primary rounded>
+					<HiStar />
+					Something!
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export default ButtonPage
