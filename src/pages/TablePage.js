e34cd393d4ce2e5a3a8f6bf38f9dbb49794332e2
Robert <rt.prominski@gmail.com>
2023-03-21 16:07:14 +0100

    create table component

---
+++ src/pages/TablePage.js
@@ -0,0 +1,29 @@
+import Table from '../components/Table'
+const TablePage = () => {
+	const config = [
+		{ label: 'Fruits', render: fruit => fruit.name },
+		{ label: 'Color', render: fruit => <div className={`p-2 m-3  ${fruit.color}`}></div> },
+		{ label: 'Score', render: fruit => fruit.score },
+	]
+
+	const data = [
+		{ name: 'Orange', color: 'bg-orange-500', score: 5 },
+		{ name: 'Apple', color: 'bg-red-300', score: 3 },
+		{ name: 'Banana', color: 'bg-yellow-500', score: 1 },
+		{ name: 'Lime', color: 'bg-green-500', score: 2 },
+	]
+
+	const keyFn = fruit => {
+		return fruit.name
+	}
+
+	return <Table data={data} config={config} keyFn={keyFn} />
+}
+
+export default TablePage
+
+// const config = [
+//     { label: 'Fruits', render: fruit => fruit.name },
+//     { label: 'Color', render: <div className={`p-2 m-3  ${fruit.color}`}></div> },
+//     { label: 'Score', render: fruit => fruit.score, sort: (a,b) => },
+// ]
